383281e45ab0304e0c2ad78a027ef9a873090440
Chandra Mohan <chandramohan0909@gmail.com>
2013-06-01 13:48:39 +0530

    Initialization of Project

---
+++ NotATinyXMLParser/NotATinyXMLParser/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  NotATinyXMLParser
+//
+//  Created by Chandra Mohan on 6/1/13.
+//  Copyright (c) 2013 org.worthydots. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
