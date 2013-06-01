46a7f2e5e10b7a45726740971de5c11bd00c62ef
Chandra Mohan <chandramohan0909@gmail.com>
2013-06-01 14:49:15 +0530

    Adding Cocos2dx library

---
+++ NotATinyXMLParser/NotATinyXMLParser/Sources/NotATinyXML.cpp
@@ -0,0 +1,9 @@
+//
+//  NotATinyXML.cpp
+//  NotATinyXMLParser
+//
+//  Created by Chandra Mohan on 6/1/13.
+//  Copyright (c) 2013 org.worthydots. All rights reserved.
+//
+
+#include "NotATinyXML.h"
---
+++ NotATinyXMLParser/NotATinyXMLParser/Sources/NotATinyXML.h
@@ -0,0 +1,14 @@
+//
+//  NotATinyXML.h
+//  NotATinyXMLParser
+//
+//  Created by Chandra Mohan on 6/1/13.
+//  Copyright (c) 2013 org.worthydots. All rights reserved.
+//
+
+#ifndef __NotATinyXMLParser__NotATinyXML__
+#define __NotATinyXMLParser__NotATinyXML__
+
+#include <iostream>
+
+#endif /* defined(__NotATinyXMLParser__NotATinyXML__) */
